f1708021e16c68cfa0f31a0b94697a8e39a64cdd
ColbyCabrera <gdthyispro@gmail.com>
2022-11-23 12:01:02 -0500

    Create js file with basic math functions and index html file

---
+++ calc.js
@@ -0,0 +1,19 @@
+function operate(operand1, operator, operand2) {
+
+}
+
+function add(op1, op2) {
+
+}
+
+function subtract(op1, op2) {
+    
+}
+
+function multiply(op1, op2) {
+    
+}
+
+function divide(op1, op2) {
+    
+}
