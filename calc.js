4b86b25de834f471993682320ec51229f5e6c661
ColbyCabrera <gdthyispro@gmail.com>
2022-11-23 12:09:22 -0500

    create function logic

--- calc.js
+++ calc.js
@@ -1,19 +1,29 @@
 function operate(operand1, operator, operand2) {
-
+    if (operator === "*") {
+        return multiply(operand1, operand2);
+    } else if (operator === "/") {
+        return divide(operand1, operand2);
+    } else if (operator === "+") {
+        return add(operand1, operand2);
+    } else {
+        return subtract(operand1, operand2);
+    }
 }
 
 function add(op1, op2) {
-
+    return op1 + op2;
 }
 
 function subtract(op1, op2) {
-    
+    return op1 - op2;
 }
 
 function multiply(op1, op2) {
-    
+    return op1 * op2;
 }
 
 function divide(op1, op2) {
-    
+    return op1 / op2;
 }
+
+console.log(operate(10, "/", 10));
